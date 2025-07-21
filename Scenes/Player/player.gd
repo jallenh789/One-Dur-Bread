c4e1572dbc9dba6085bbb0a3dcb3dffa4ad3b218
jallenh789 <jallenh789@gmail.com>
2025-07-20 22:22:06 -0500

    added basic movement to the player
    within the player script:
    
    within input map created actions and binded keys to them:
    move_up:   (w,^)
    move_down: (s,v)
    move_right:(d,>)
    move_left: (a,<)
    
    created the move_vector var
    created and exported the move_speed
    added player velocity
    
    Player velocity is being changed by the inputs form move_vector and then multiplied by the move_speed variable
    
    player
       L_ charecterbody2d motion mode was changed to floating so godot knows not to apply downward physics on the player
    
    Created the initial game_scene.tcn and game scene folder in scenes
    
    added camera2d to player

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+# used to change the player speed 
+@export var move_speed: int = 50
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = move_vector * move_speed
+	
+	move_and_slide()
